685b10307013b8a96b3ec909bd0a4fbe0a60f4f8
Lorenzo Padoan <lorenzo.padoan977@gmail.com>
2019-03-08 12:11:11 +0100

    Update and rename es1B.c to srd.c

--- srd.c
+++ srd.c
@@ -1,6 +1,6 @@
-#include "es1B.h"
 
-double radice_quadrata(double x, double epsilon) {
+//square root with dichotomy method
+double square_root(double x, double epsilon) {
 	double a = 1;
 	double b = x;
 	double med;
